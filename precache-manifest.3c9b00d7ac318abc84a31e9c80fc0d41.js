58de46326c319dbd2126e6eefcbadb3a6100bc64
Alexandr Zhuk <sanek201dg@ukr.net>
2019-02-11 12:02:23 +0200

    Updates

---
+++ precache-manifest.3c9b00d7ac318abc84a31e9c80fc0d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a471af0c98aeb6162f3",
+    "url": "/Excel_Table_App/static/js/runtime~main.5a471af0.js"
+  },
+  {
+    "revision": "33d79fc3040a2b3e6f98",
+    "url": "/Excel_Table_App/static/js/main.33d79fc3.chunk.js"
+  },
+  {
+    "revision": "38a76cbe74a6aa09f65d",
+    "url": "/Excel_Table_App/static/js/1.38a76cbe.chunk.js"
+  },
+  {
+    "revision": "33d79fc3040a2b3e6f98",
+    "url": "/Excel_Table_App/static/css/main.68ba87f7.chunk.css"
+  },
+  {
+    "revision": "064f597435fb61910b96b08631839f4a",
+    "url": "/Excel_Table_App/index.html"
+  }
+];
